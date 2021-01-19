d19ec97ef53a8ae26b2dc1ce7b5523fbe5ef40e7
MartialEscudero <martial.escudero@gmail.com>
2021-01-19 17:57:45 +0100

    Modification de la donnée IsDone

--- src/store.js
+++ src/store.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
+axios.get('https://strapi.hortusbox.com/todos').then(response => {
+  console.log(response.data[1].IsDone);
+});
+
 export default new Vuex.Store({
     state: {
         todoShop : [],
@@ -20,21 +24,54 @@ export default new Vuex.Store({
             }
         },
 
+
         DELETELISTSHOP(state, todoShop) {
+
             var index = state.todoShop.indexOf(todoShop);
             state.todoShop.splice(index, 1);
+            
+            axios.get('https://strapi.hortusbox.com/todos').then(response => {
+
+                var alldata = response.data;
+
+                alldata.forEach(element => {
+
+                    if (todoShop == element.Task) {
+
+                        var id = element.id;
+
+                        axios.put('https://strapi.hortusbox.com/todos/'+id, {IsDone: true});
+                    }
+
+                });
+                
+            });
+
         },
 
+
         GETTODOS(state) {
+
             if (state.todoShop.length != 0 ) {
                 state.todoShop.clear();
             }
 
             axios.get('https://strapi.hortusbox.com/todos').then(response => {
+                
                 var data = response.data;
-                data.forEach(element => state.todoShop.push(element.Task));
+
+                data.forEach(element => {
+
+                    if (element.IsDone == false) {
+                        state.todoShop.push(element.Task);
+                    }
+
+                });
+
             });
+
         },
+
 
         ADDLISTSIMPLE(state, todoSimple) {
 
@@ -44,9 +81,12 @@ export default new Vuex.Store({
             }
         },
 
+
         DELETELISTSIMPLE(state, todoSimple) {
+
             var index = state.todoSimple.indexOf(todoSimple);
             state.todoSimple.splice(index, 1);
-        },
+
+        }
     }
 })
